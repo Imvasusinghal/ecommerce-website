0e1cfffe7135a0ca22ca38905f713f9ce92ae313
Imvasusinghal <80517041+Imvasusinghal@users.noreply.github.com>
2023-07-26 15:10:19 +0530

    Updated error comming while deployment

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors';
+import path from "path";
+import { fileURLToPath } from 'url';
 
 //CONFIGURE env
 dotenv.config();
 
 //DATABASE CONFIG
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //REST OBJECT
 const app = express();
